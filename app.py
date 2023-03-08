c55a8b1ed1ad989ed7654d58f1c4ed925533cf1a
Prakashraj <prakashraj15.tn@gmail.com>
2023-03-08 18:42:16 +0530

    recon phantom

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
 from chat import get_response
-
+from gpt import get_reply
 app = Flask(__name__)
 
 
@@ -9,15 +9,21 @@ app = Flask(__name__)
 def index_get():
     return render_template("base.html")
 
+@app.post("/openAi")
+def openAi():
+    text = request.get_json().get("message")
+    if(text=='open ai'):
+        return None
+    response = get_reply(text)
+    message = {"answer": response}
+    return jsonify(message)
 
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
-
+    response1 = get_response(text)
+    message1 = {"answer": response1}
+    return jsonify(message1)
 
 if __name__ == "__main__":
     app.run(debug=True)
